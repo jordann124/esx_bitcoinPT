ac2276a7fb02b95b56a23f0e1747d7d864178752
Jordan <51269322+jordann124@users.noreply.github.com>
2019-06-23 22:46:33 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales['en'] = {
+	['carrega_para_apanhares_frutos'] = 'Carrega ~INPUT_CONTEXT~ para apanahres Bitcoin',
+	['carrega_para_venderes_os_sacos'] = 'Carrega ~INPUT_CONTEXT~ para vender as BitCoin',
+	['mochila_full'] = 'Tu não podes pegar mais Bicoin porque esta cheia a tua ~r~mochila~s~',
+	['pegar_frutos'] = '~y~A Minerar Bitcoin~s~...',
+	['nao_tens_mais_frutos'] = 'Não Tens Mais ~r~Bicoin~s~',
+	['nao_tens_sacos_com_frutos'] = 'Não Tens Mais ~r~Bitcoin~s~',
+	['vendeste_sacos'] = 'Vendeste ~y~x1~s~ ~y~Bitcoin~s~',
+	['venda_do_sacos'] = '~g~A Vender Bitcoin~s~...',
+}
